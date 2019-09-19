8e0fd2701790f97f4cef0ca51382a4a5b478334b
Rectmoon <15692087320@163.com>
2019-09-20 05:29:41 +0800

    抽象

--- app.js
+++ app.js
@@ -9,9 +9,3 @@ app.use(express.static(path.join(__dirname, 'dist'), { maxAge: '10m' }))
 app.listen(port, () => {
   console.log(`Application is listening at http://localhost:${port}`)
 })
-
-console.log(123)
-
-console.log(123456)
-
-console.log(56789)
--- src/a.js
+++ src/a.js
@@ -1,7 +1,12 @@
-require('./modules/t')
+// require('./modules/t')
 
-if (module.hot) {
-  module.hot.accept(['./modules/t.js'], () => {
-    require('./modules/t.js').default
-  })
+// if (module.hot) {
+//   module.hot.accept(['./modules/t.js'], () => {
+//     require('./modules/t.js').default
+//   })
+// }
+
+console.log(process.env.NODE_ENV)
+if (process.env.NODE_ENV === 'development') {
+  console.log('Welcome to development')
 }
